fa0782925315cbcf20b38f5a4590330fba68176c
khylegodwinmagallano-sudo <khylegodwin.magallano@hcdc.edu.ph>
2025-09-21 20:57:02 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "                 OO     ALWAYS" << endl;
+    cout << "                OO      CHECK" << endl;
+    cout << "               OO       YOUR" << endl;
+    cout << "              OO        FRIENDS!" << endl;
+    cout << "             OO" << endl;
+    cout << "            OO" << endl;
+    cout << "           OO" << endl;
+    cout << "OO        OO" << endl;
+    cout << " OO      OO" << endl;
+    cout << "  OO    OO" << endl;
+    cout << "   OO  OO" << endl;
+    cout << "    OOOO" << endl;
+    cout << "     OO" << endl;
+    return 0;
+}
